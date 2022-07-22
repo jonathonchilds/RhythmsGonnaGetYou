ba1ad319f5db81a907f4b3dd8bc1fb04cdc5a406
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-07-22 11:52:00 -0600

    finishes working modify databse menu

--- Program.cs
+++ Program.cs
@@ -6,6 +6,118 @@ namespace RhythmsGonnaGetYou
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            var context = new RhythmsGonnaGetYouContext();
+
+            var keepGoing = true;
+            while (keepGoing)
+            {
+                Console.WriteLine(" (M)odify Database");
+                Console.WriteLine(" (V)iew Database ");
+                Console.WriteLine(" View all of the (b)ands");
+                Console.WriteLine(" (E)nter a bands name to view all of their albums.");
+                Console.WriteLine(" View all albums ordered by (r)elease date.");
+                Console.WriteLine(" View all bands that are (s)igned.");
+                Console.WriteLine(" View all bands that are (n)ot signed.");
+                Console.WriteLine(" (Q)uit Program.");
+
+                var choice = Console.ReadLine().ToLower();
+
+                switch (choice)
+                {
+                    case "m":
+                        Console.WriteLine("Would you like to:");
+                        Console.WriteLine(" (A)dd a new band.");
+                        Console.WriteLine(" Add an al(b)um for a band.");
+                        Console.WriteLine(" Add a (s)ong to an album.");
+                        Console.WriteLine(" (L)et a band go. (update isSigned to false)");
+                        Console.WriteLine(" (R)esign a band. (update isSigned to true)");
+                        var modification = Console.ReadLine().ToLower();
+
+                        if (modification == "a")
+                        {
+                            AddBand(context);
+                        }
+                        else if (modification == "b")
+                        {
+                            AddAlbum(context);
+                        }
+                        else if (modification == "s")
+                        {
+                            AddSong(context);
+                        }
+                        else if (modification == "l")
+                        {
+                            UnsignBand(context);
+                        }
+                        else if (modification == "r")
+                        {
+                            SignBand(context);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, that's not a valid input.");
+                        }
+                        break;
+
+                    case "v":
+
+
+                    case "q":
+                        keepGoing = false;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("That is not a valid response. Please try again.");
+                        break;
+
+                }
+            }
+        }
+
+        public static void SignBand(RhythmsGonnaGetYouContext context)
+        {
+            var bandToUpdate = PromptForString("Which band is being signed?");
+            var band = context.Bands.FirstOrDefault(band => band.Name == bandToUpdate);
+            var boolToUpdate = PromptForBool($"You will be signing {band.Name}, correct?");
+            band.IsSigned = boolToUpdate;
+            context.Bands.Update(band);
+            context.SaveChanges();
+            Console.WriteLine($"{band.Name} is now signed.");
+        }
+
+        public static void UnsignBand(RhythmsGonnaGetYouContext context)
+        {
+            var bandToUpdate = PromptForString("Which band is being let go?");
+            var band = context.Bands.FirstOrDefault(band => band.Name == bandToUpdate);
+            var boolToUpdate = PromptForBool($"You will be letting go of {band.Name}, correct?");
+            band.IsSigned = !boolToUpdate;
+            context.Bands.Update(band);
+            context.SaveChanges();
+            Console.WriteLine($"{band.Name} is now unsigned.");
+        }
+
+        public static void AddBand(RhythmsGonnaGetYouContext context)
+        {
+            var newBand = new Band
+            {
+                Name = PromptForString($"What is the name of the band you're adding? "),
+                CountryOfOrigin = PromptForString($"What country is the band from? "),
+                NumberOfMembers = PromptForInteger($"How many members are in this band? "),
+                Website = PromptForString("Enter the web address for this band: "),
+                Style = PromptForString("What style/genre of music does this band play? "),
+                IsSigned = PromptForBool("Is this band signed? (Y/N) "),
+                ContactName = PromptForString("Who is the contact point (agent) for this band? "),
+                ContactPhoneNumber = PromptForString("Enter the phone number for the agent: ((555) 555-5555) ")
+            };
+            context.Bands.Add(newBand);
+            context.SaveChanges();
+            Console.WriteLine($"{newBand.Name} was added!");
+        }
+
         static string PromptForString(string prompt)
         {
             Console.WriteLine(prompt);
@@ -32,7 +144,6 @@ namespace RhythmsGonnaGetYou
                 Console.WriteLine("Sorry, that isn't a valid input. I'm using 0 as your answer. ");
                 return 0;
             }
-
         }
 
         static bool PromptForBool(string prompt)
@@ -47,144 +158,50 @@ namespace RhythmsGonnaGetYou
 
         }
 
-        // public static string ToStringUtc(this DateTime time)
-        //{
-        //  return $"DateTime({time.Ticks}, DateTimeKind.Utc)";
-        //}
-
-
-        static void Main(string[] args)
+        public static void AddSong(RhythmsGonnaGetYouContext context)
         {
-            var context = new RhythmsGonnaGetYouContext();
+            var albumToUpdate = PromptForString("For which album would you like to add a song? ");
+            var album = context.Albums.FirstOrDefault(album => album.Title == albumToUpdate);
 
-            // var host = .Build();
-            //AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-            var keepGoing = true;
-            while (keepGoing)
+            var newSong = new Song
             {
-                Console.WriteLine(" (M)odify Database");
-                Console.WriteLine(" (V)iew Database ");
-                Console.WriteLine(" View all of the (b)ands");
-                Console.WriteLine(" (E)nter a bands name to view all of their albums.");
-                Console.WriteLine(" View all albums ordered by (r)elease date.");
-                Console.WriteLine(" View all bands that are (s)igned.");
-                Console.WriteLine(" View all bands that are (n)ot signed.");
-                Console.WriteLine(" (Q)uit Program.");
+                Title = PromptForString("Enter the name of the song you'd like to add: "),
+                TrackNumber = PromptForInteger("Enter the track number: "),
+                Duration = PromptForString("Enter the track's duration: "),
+                AlbumId = album.Id
+            };
 
-                var choice = Console.ReadLine().ToLower();
+            context.Songs.Add(newSong);
+            context.SaveChanges();
+            Console.WriteLine($"{newSong.Title} was added to {albumToUpdate}!");
+        }
 
-                switch (choice)
-                {
-                    case "m":
-                        Console.WriteLine("Would you like to:");
-                        Console.WriteLine(" (A)dd a new band.");
-                        Console.WriteLine(" Add an al(b)um for a band.");
-                        Console.WriteLine(" Add a (s)ong to an album.");
-                        Console.WriteLine(" (L)et a band go. (update isSigned to false)");
-                        Console.WriteLine(" (R)esign a band. (update isSigned to true)");
-                        var modification = Console.ReadLine().ToLower();
-                        if (modification == "a")
-                        {
-
-                            var newName = PromptForString($"What is the name of the band you're adding? ");
-                            var newCountryOfOrigin = PromptForString($"What country is the band from? ");
-                            var newNumberOfMembers = PromptForInteger($"How many members are in this band? ");
-                            var newWebsite = PromptForString("Enter the web address for this band: ");
-                            var newStyle = PromptForString("What style/genre of music does this band play? ");
-                            var newIsSigned = PromptForBool("Is this band signed? (Y/N) ");
-                            var newContactName = PromptForString("Who is the contact point (agent) for this band? ");
-                            var newContactNumber = PromptForString("Enter the phone number for the agent: ((555) 555-5555) ");
-
-                            var newBand = new Band
-                            {
-                                Name = newName,
-                                CountryOfOrigin = newCountryOfOrigin,
-                                NumberOfMembers = newNumberOfMembers,
-                                Website = newWebsite,
-                                Style = newStyle,
-                                IsSigned = newIsSigned,
-                                ContactName = newContactName,
-                                ContactPhoneNumber = newContactNumber
-                            };
-
-                            context.Bands.Add(newBand);
-                            context.SaveChanges();
-                            Console.WriteLine($"{newName} was added!");
+        public static void AddAlbum(RhythmsGonnaGetYouContext context)
+        {
+            var nameToUse = PromptForString("For which band would you like to add an album?");
+            var band = context.Bands.FirstOrDefault(band => band.Name == nameToUse);
+            Console.WriteLine("Enter the release date for the album (YYYY-MM-DD) ");
+            var releaseDate = DateTimeToUTC();
 
 
-                        }
-                        else if (modification == "b")
-                        {
-                            var nameToUse = PromptForString("For which band would you like to add an album?");
-                            var band = context.Bands.FirstOrDefault(band => band.Name == nameToUse);
-                            var albumNameToAdd = PromptForString("Please enter the name of the album you'd like to add: ");
-                            var isExplicit = PromptForBool("Is the album explicit? ");
-                            Console.WriteLine("When was the album released? (YYYY-MM-DD) ");
-                            var inputValue = DateTime.Parse(Console.ReadLine());
-                            var inputValueInUTC = inputValue.ToUniversalTime();
+            var newAlbum = new Album
+            {
+                Title = PromptForString("Please enter the name of the album you'd like to add: "),
+                IsExplicit = PromptForBool("Is the album explicit? "),
+                ReleaseDate = releaseDate,
+                BandId = band.Id
+            };
 
+            context.Albums.Add(newAlbum);
+            context.SaveChanges();
+            Console.WriteLine($"{newAlbum.Title} was added to {band}!");
+        }
 
-                            var newAlbum = new Album
-                            {
-                                Title = albumNameToAdd,
-                                IsExplicit = isExplicit,
-                                ReleaseDate = inputValueInUTC,
-                                BandId = band.Id
-                            };
-
-                            context.Albums.Add(newAlbum);
-                            context.SaveChanges();
-                            Console.WriteLine($"{albumNameToAdd} was added to {band}!");
-
-                        }
-
-                        else if (modification == "s")
-
-                        {
-                            var albumToUpdate = PromptForString("For which album would you like to add a song? ");
-                            var album = context.Albums.FirstOrDefault(album => album.Title == albumToUpdate);
-                            var songTitle = PromptForString("Enter the name of the song you'd like to add: ");
-                            var trackNumber = PromptForInteger("Enter the track number: ");
-                            var duration = PromptForString("Enter the track's duration: ");
-
-                            var newSong = new Song
-                            {
-                                Title = songTitle,
-                                TrackNumber = trackNumber,
-                                Duration = duration,
-                                AlbumId = album.Id
-                            };
-
-                            context.Songs.Add(newSong);
-                            context.SaveChanges();
-                            Console.WriteLine($"{songTitle} was added to {albumToUpdate}!");
-                        }
-
-
-                        else
-                        {
-                            Console.WriteLine("Sorry, that's not a valid input.");
-                        }
-
-                        break;
-
-                    case "q":
-                        keepGoing = false;
-                        break;
-
-                    default:
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine("That is not a valid response. Please try again.");
-                        break;
-
-
-
-
-
-                }
-            }
+        public static DateTime DateTimeToUTC()
+        {
+            var inputValue = DateTime.Parse(Console.ReadLine());
+            var inputValueInUTC = inputValue.ToUniversalTime();
+            return inputValueInUTC;
         }
     }
 }
